e81e92371d9dd9ef8bfc44b06f04ab44f172af49
Anjugadwal84 <Anju_gadwal84@yahoo.com>
2022-02-01 12:07:50 +0530

    first commit

---
+++ Core Practice/ArrayAssignment.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+class ArrayAssignment
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the size of an Array: ");
+		int size = scan.nextInt();
+		// Array A
+		int a[] = new int[size];
+		System.out.println("Enter an elements of Array A : ");
+		for(int i = 0; i<size;i++)
+		{
+			
+			a[i] = scan.nextInt();
+		}
+		System.out.println("Entered elements are : " );
+		for(int i = 0; i<a.length;i++)
+		{
+			
+			System.out.println(a[i]);
+		} 
+	
+		
+	 	// Elements are in Ascending Order 
+
+		int temp;
+		for(int i =0; i<a.length; i++)
+		{	
+			
+			for(int j =0; j<a.length-1-i; j++)
+			{
+				if(a[j] > a[j+1])
+				{
+					temp = a[j];
+					a[j]=a[j+1];
+					a[j+1] = temp;
+					
+				}
+			}
+		
+				
+		}
+		System.out.println("Elements are in Ascending Order  : ");
+		for(int i=0; i<a.length; i++)
+		
+		{
+			
+			System.out.println(a[i]);
+		}
+		
+
+		// Elements are in Descending Order 
+		int temp1;
+		for(int i =0; i<a.length; i++)
+		{	
+			
+			for(int j =0; j<a.length-1-i; j++)
+			{
+				if(a[j] < a[j+1])
+				{
+					temp1 = a[j];
+					a[j]=a[j+1];
+					a[j+1] = temp1;
+					
+				}
+			}
+		
+				
+		}
+		System.out.println("Elements are in Descending Order  : ");
+		for(int i=0; i<a.length; i++)
+		
+		{
+			
+			System.out.println(a[i]);
+		}
+		
+		// Searching 
+		int item; 
+		System.out.println("Enter the value of Item : ");
+		item = scan.nextInt();
+		for (int i = 0; i<a.length; i++)
+		{
+			if(a[i]==item)
+			{
+				System.out.println("Element is present at :  " + i + "   Index Position");
+			}
+		}	
+		
+
+		
+		// Insertion  
+		
+		int position; 
+		System.out.println("Enter the value of position : ");
+		position = scan.nextInt();
+		int element;
+		System.out.println("Enter the value of Element : ");
+		element= scan.nextInt();
+
+		for(int i=a.length-1; i>position-1; i--)
+		{
+			a[i]= a[i-1];
+		}
+		a[position-1]=element;
+		System.out.println("Array after insertion of an element  : ");
+		for (int i = 0; i<a.length; i++)
+		{
+			
+			{
+				System.out.println(a[i]);
+			}
+		}	
+
+		
+			
+	}
+}
